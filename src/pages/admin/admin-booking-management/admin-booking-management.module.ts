dae403c8cac5265aecfd02421f3978b98960a44d
Unknown <farwaj1@gmail.com>
2018-12-20 15:19:32 +0000

    Translations added to admin booking management and Registration

--- src/pages/admin/admin-booking-management/admin-booking-management.module.ts
+++ src/pages/admin/admin-booking-management/admin-booking-management.module.ts
@@ -1,3 +1,4 @@
+import { TranslateModule } from '@ngx-translate/core';
 import { ComponentsModule } from './../../../components/components.module';
 import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
@@ -9,6 +10,7 @@ import { AdminBookingManagementPage } from './admin-booking-management';
   ],
   imports: [
     ComponentsModule,
+    TranslateModule.forChild(),
     IonicPageModule.forChild(AdminBookingManagementPage),
   ],
 })
