3fa91b77f95544ebf837e64789a3227366e2d086
Unknown <farwaj1@gmail.com>
2019-01-31 21:46:13 +0000

    Replaced Hard coded Data From Assign to Tutor Page

--- src/pages/admin/admin-booking-assign/admin-booking-assign.ts
+++ src/pages/admin/admin-booking-assign/admin-booking-assign.ts
@@ -1,29 +1,62 @@
+import { UserInfoService } from './../../../services/UserInfo.provider';
+import { UserService } from './../../../services/User.provider';
 import { Booking } from './../../../class/Booking';
-import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
-
-/**
- * Generated class for the AdminBookingAssignPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
+import { Component, OnInit } from '@angular/core';
+import { IonicPage, NavController, NavParams, ToastController } from 'ionic-angular';
+import { Principal } from '../../../providers/auth/principal.service';
+import { User } from '../../../class/User';
+import { UserInfo } from '../../../class/UserInfo';
 
 @IonicPage()
 @Component({
   selector: 'page-admin-booking-assign',
   templateUrl: 'admin-booking-assign.html',
 })
-export class AdminBookingAssignPage {
-
+export class AdminBookingAssignPage implements OnInit {
   selectedBooking: Booking;
+  filterTutors: Array<User>=[];
+  userInfos: Array<UserInfo>;
 
-
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, private userService: UserService, private toastCtrl: ToastController, private principal: Principal, private userInfoService: UserInfoService) {
     if (this.navParams.get("selectedBooking") != null || this.navParams.get("selectedBooking") != undefined) {
       this.selectedBooking = this.navParams.get("selectedBooking");
       console.log("Selected booking to assign to tutor : ", this.selectedBooking);
     }
   }
 
+  ngOnInit() {
+    this.initUsers();
+    this.initUserInfo();
+  }
+
+  initUsers() {
+    this.userService.query().subscribe(
+      (response) => {
+        response.forEach(user => {
+          user.authorities.forEach(authority => {
+            if (authority == "ROLE_TUTOR") {
+              this.filterTutors = this.filterTutors.concat(user);
+            }
+          });
+        });
+
+      },
+      (error) => {
+        console.error(error);
+        let toast = this.toastCtrl.create({ message: 'Failed to load data', duration: 2000, position: 'middle' });
+        toast.present();
+      });
+  }
+
+  initUserInfo() {
+    this.userInfoService.query().subscribe(
+      (response) => {
+        this.userInfos=response;
+      },
+      (error) => {
+        console.error(error);
+        let toast = this.toastCtrl.create({ message: 'Failed to load data', duration: 2000, position: 'middle' });
+        toast.present();
+      });
+  }
 }
