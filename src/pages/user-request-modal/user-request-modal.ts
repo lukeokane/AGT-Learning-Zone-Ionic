d788fc76e40f4b733745b9fcacf31bef67c4557d
elaine chong <elainepeilingchong@gmail.com>
2019-01-31 18:29:25 +0000

    User request page 1

--- src/pages/user-request-modal/user-request-modal.ts
+++ src/pages/user-request-modal/user-request-modal.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, ModalController } from 'ionic-angular';
+import { UserRequestTimeslotPage } from '../user-request-timeslot/user-request-timeslot';
+import { Booking } from '../../class/Booking';
 
 /**
  * Generated class for the UserRequestModalPage page.
@@ -16,17 +18,54 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 export class UserRequestModalPage {
   dateSelected: Date;
   timeSelected: any;
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  visible1: any;
+  visible2: any;
+  visible3: any;
+  booking:Booking;
+
+  constructor(public navCtrl: NavController, public navParams: NavParams,private modalCtrl:ModalController) {
     this.dateSelected = this.navParams.get("dateSelected");
     console.log(this.dateSelected.toString());
     this.timeSelected = this.navParams.get("timeSelected");
+    this.visible1 = true;
+    this.visible2 = true;
+    this.visible3 = true;
+
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad SlotModalPage');
   }
-  convertToString(){
-    return "Date: "+ this.dateSelected.getDate() +"/"+(this.dateSelected.getMonth()+1)+ " Time:"+ this.timeSelected+":00"; 
+  convertToString() {
+    return "Date: " + this.dateSelected.getDate() + "/" + (this.dateSelected.getMonth() + 1) + " Time:" + this.timeSelected + ":00";
   }
 
+  onClickPriority(tyle: String, visible: any) {
+    if (visible == 1) {
+      this.visible1 = false;
+      this.visible2 = true;
+      this.visible3 = true;
+
+    } else if (visible == 2) {
+      this.visible2 = false;
+      this.visible1 = true;
+      this.visible3 = true;
+
+
+    } else if (visible == 3) {
+      this.visible3 = false;
+      this.visible1 = true;
+      this.visible2 = true;
+    }
+
+  }
+
+  onClickContinue(){
+    console.log("CLICKED");
+    let timeSlotModal = this.modalCtrl.create(UserRequestTimeslotPage, {booking: this.booking });
+    timeSlotModal.onDidDismiss(data => {
+      console.log(data);
+    });
+    timeSlotModal.present();
+  }
 }
