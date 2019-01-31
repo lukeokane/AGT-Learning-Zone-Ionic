8c27d14896d1642b28a64fecebbee2fb68b87700
elaine chong <elainepeilingchong@gmail.com>
2019-01-31 18:29:01 +0000

    Removed unused line

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -72,7 +72,6 @@ export class HomePage implements OnInit {
   slotClicked(dateSelected: Date, timeSelected: String) {
     var time = timeSelected.substring(0, 2);
     var timeInt = parseInt(time, 10);
-    console.log(dateSelected);
     let profileModal = this.modalCtrl.create(UserRequestModalPage, { dateSelected: dateSelected, timeSelected: timeInt });
     profileModal.onDidDismiss(data => {
       console.log(data);
