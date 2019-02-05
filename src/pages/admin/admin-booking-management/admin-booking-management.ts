e3cc05ec52ecdc5ac65a2cfc837ca2cf08dcb423
Unknown <farwaj1@gmail.com>
2019-02-05 17:14:16 +0000

    Assign randomly

--- src/pages/admin/admin-booking-management/admin-booking-management.ts
+++ src/pages/admin/admin-booking-management/admin-booking-management.ts
@@ -4,12 +4,14 @@ import { SemesterGroup } from './../../../class/SemesterGroup';
 import { UserService } from './../../../services/User.provider';
 import { HttpResponse } from '@angular/common/http';
 import { BookingsService } from './../../../services/Booking.provider';
-import { adminBookingAssignnPage } from './../../pages';
+import { adminBookingAssignnPage, homePage } from './../../pages';
 import { Booking } from './../../../class/Booking';
 import { Component, OnInit } from '@angular/core';
 import { IonicPage, NavController, NavParams, ToastController } from 'ionic-angular';
 import { User } from '../../../class/User';
 import { Subject } from '../../../class/Subject';
+import { UserInfo } from '../../../class/UserInfo';
+import { UserInfoService } from '../../../services/UserInfo.provider';
 
 @IonicPage()
 @Component({
@@ -34,21 +36,22 @@ export class AdminBookingManagementPage implements OnInit {
   predicate: any = 'id';
   previousPage: any;
   reverse: any;
+  userInfos: Array<UserInfo>;
+  filterTutors: Array<User> = [];
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, private bookingsService: BookingsService, private toastCtrl: ToastController, private userService: UserService, private semesterGroupService: SemesterGroupService, private subjectService: SubjectsService) {
-    // this.bookings=[new Booking(1,"help in Java","Shaun Conroy", new Date("Tue Oct 23 2018 10:00:00"), new Date("Tue Oct 23 2018 11:00:00"),"I'm trying to learn the best way to implement interfaces with inheritance for a topic being taught in Object Oriented Programming next week",OrdinalScale.LOW,null,null,null,null,null,null,new Subject(1,null,"Java",[new Topic(1,"OOP")],null,null),[new UserInfo(1,null,new User(1,null,null,"Shaun","Conroy","D001893736"),new SemesterGroup(1,"Group 1",null,[new Subject(1,"Java","Java",[new Topic(1,"Classes",null,null)],null,null)],null,null),null,null,null,null)],null),new Booking(1,"help in Java","Shaun Conroy", new Date("Tue Oct 23 2018 10:00:00"), new Date("Tue Oct 23 2018 11:00:00")," is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s",OrdinalScale.MEDIUM,null,null,null,null,null,null,new Subject(1,null,"Java",null,null,null),[new UserInfo(1,null,new User(1,null,null,"Elaine Pei","Ling Chong","D0018910736"),new SemesterGroup(1,"dd",null,[new Subject(1,"Java","Java",[new Topic(1,"Classes",null,null)],null,null)],null,null),null,null,null,null)],null),new Booking(1,"help in Java","Shaun Conroy", new Date("Tue Oct 23 2018 10:00:00"), new Date("Tue Oct 23 2018 11:00:00"),"There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour",OrdinalScale.HIGH,null,null,null,null,null,null,new Subject(1,null,"Java",null,null,null),[new UserInfo(1,null,new User(1,null,null,"Luke","O'Kane","D0017209171"),new SemesterGroup(1,"dd",null,[new Subject(1,"Java","Java",[new Topic(1,"Classes",null,null)],null,null)],null,null),null,null,null,null)],null)];
+  constructor(public navCtrl: NavController, public navParams: NavParams, private bookingsService: BookingsService, private toastCtrl: ToastController, private userService: UserService, private semesterGroupService: SemesterGroupService, private subjectService: SubjectsService, private userInfoService: UserInfoService, private bookingService: BookingsService) {
   }
 
   ngOnInit() {
     this.initBooking();
     this.initUsersInfo();
+    this.initUserInfo();
     this.initSemesterGroup();
     this.initSubjects();
   }
 
   initBooking(refresher?) {
     this.itemsPerPage = 4;
-
     this.bookingsService.findBookingsPendingAdminApproval({
       page: this.page - 1,
       size: this.itemsPerPage,
@@ -63,30 +66,6 @@ export class AdminBookingManagementPage implements OnInit {
         toast.present();
       });
   }
-
-  private onSuccess(data, headers) {
-    this.totalItems = headers.get('X-Total-Count');
-    this.queryCount = this.totalItems;
-    this.bookings = data;
-
-    console.log("Bookings", this.bookings);
-  }
-
-  loadPage(page: number) {
-    if (page !== this.previousPage) {
-      this.previousPage = page;
-      this.initBooking();
-    }
-  }
-
-  sort() {
-    const result = [this.predicate + ',' + (this.reverse ? 'asc' : 'desc')];
-    if (this.predicate !== 'id') {
-      result.push('id');
-    }
-    return result;
-  }
-
 
   initUsersInfo() {
     this.userService.query().subscribe(
@@ -116,12 +95,6 @@ export class AdminBookingManagementPage implements OnInit {
       });
   }
 
-  onSuccess1(data, headers) {
-    this.totalItems = headers.get('X-Total-Count');
-    this.queryCount = this.totalItems;
-    this.semesterGroups = data;
-    console.log("SemesterGroup", this.semesterGroups);
-  }
 
   initSubjects() {
     this.itemsPerPage = 125;
@@ -139,11 +112,53 @@ export class AdminBookingManagementPage implements OnInit {
       });
   }
 
+  initUserInfo() {
+    this.userInfoService.query().subscribe(
+      (response) => {
+        this.userInfos = response;
+      },
+      (error) => {
+        console.error(error);
+        let toast = this.toastCtrl.create({ message: 'Failed to load data', duration: 2000, position: 'middle' });
+        toast.present();
+      });
+  }
+
+  private onSuccess(data, headers) {
+    this.totalItems = headers.get('X-Total-Count');
+    this.queryCount = this.totalItems;
+    this.bookings = data;
+
+    // console.log("Bookings", this.bookings);
+  }
+
+  onSuccess1(data, headers) {
+    this.totalItems = headers.get('X-Total-Count');
+    this.queryCount = this.totalItems;
+    this.semesterGroups = data;
+    // console.log("SemesterGroup", this.semesterGroups);
+  }
+
   onSuccess3(data, headers) {
     this.totalItems = headers.get('X-Total-Count');
     this.queryCount = this.totalItems;
     this.subjects = data;
-    console.log("Subjects", this.subjects);
+    // console.log("Subjects", this.subjects);
+  }
+
+  loadPage(page: number) {
+    if (page !== this.previousPage) {
+      this.previousPage = page;
+      this.initBooking();
+    }
+  }
+
+  sort() {
+    const result = [this.predicate + ',' + (this.reverse ? 'asc' : 'desc')];
+    if (this.predicate !== 'id') {
+      result.push('id');
+    }
+    return result;
   }
 
   goToBooking(booking: Booking) {
@@ -151,10 +166,42 @@ export class AdminBookingManagementPage implements OnInit {
     // this.dayOfWeek= this.selectedBooking.startTime.getDay() ==0 ? "Sunday" : this.selectedBooking.startTime.getDay()==1? "Monday" : this.selectedBooking.startTime.getDay()== 2 ? "Tuesday" : this.selectedBooking.startTime.getDay()== 3 ? "Wednesday" :  this.selectedBooking.startTime.getDay()== 4 ? "Thursday" :  this.selectedBooking.startTime.getDay()== 5 ? "Friday" : this.selectedBooking.startTime.getDay()== 6 ? "Saturday"  : void 0;
   }
 
-  goToAssignTutorManually(selectedBooking:Booking) {
-    this.navCtrl.push(adminBookingAssignnPage,{
-      selectedBooking:selectedBooking
+  goToAssignTutorManually(selectedBooking: Booking) {
+    this.navCtrl.push(adminBookingAssignnPage, {
+      selectedBooking: selectedBooking
     });
+  }
+
+  assignTutorRandomly(selectedBooking: Booking) {
+    this.userService.query().subscribe(
+      (response) => {
+        response.forEach(user => {
+          user.authorities.forEach(authority => {
+            if (authority == "ROLE_TUTOR") {
+              this.filterTutors = this.filterTutors.concat(user);
+            }
+          });
+        });
+        this.assignToTutor(selectedBooking, this.filterTutors);
+      },
+      (error) => {
+        console.error(error);
+        let toast = this.toastCtrl.create({ message: 'Failed to load data', duration: 2000, position: 'middle' });
+        toast.present();
+      });
+
+  }
+
+  assignToTutor(selectedBooking: Booking, filterTutors: Array<User> = []) 
+  {
+    let rand = filterTutors[Math.floor(Math.random() * filterTutors.length)];
+    if (rand != null || rand != undefined) {
+      selectedBooking.adminAcceptedId = rand.id;
+    }
+    if (selectedBooking != null || selectedBooking != undefined) {
+      this.bookingService.saveBooking(selectedBooking);
+      this.navCtrl.push(homePage);
+    }
   }
 
 
