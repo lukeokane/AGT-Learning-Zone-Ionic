b6a0d4407a086ce298d2e6d65ad9cd8b79f98fca
elaine chong <elainepeilingchong@gmail.com>
2019-02-10 21:17:55 +0000

    Modified User-home page

--- src/components/top-menu/top-menu.ts
+++ src/components/top-menu/top-menu.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
-import { PopoverController } from 'ionic-angular';
+import { PopoverController, App } from 'ionic-angular';
+import { AuthServerProvider } from '../../providers/auth/auth-jwt.service';
+import { loginPage, FirstRunPage } from '../../pages/pages';
 
 @Component({
   selector: 'top-menu',
@@ -9,12 +11,14 @@ export class TopMenuComponent {
 
   text: string;
 
-  constructor(public popoverCtrl: PopoverController) {
+  constructor(public popoverCtrl: PopoverController,
+    private authServerProvider: AuthServerProvider,
+    private app: App) {
   }
 
   showNotifications(myEvent) {
     console.log(myEvent);
-    const popover = this.popoverCtrl.create("PopoverNotificationPage",{},{cssClass:'custom-popover'});
+    const popover = this.popoverCtrl.create("PopoverNotificationPage", {}, { cssClass: 'custom-popover' });
     // myEvent={
     //   target : {
     //     getBoundingClientRect : () => {
@@ -24,8 +28,21 @@ export class TopMenuComponent {
     //     }
     //   }
     // };
-    
+
     // const popover = this.popoverCtrl.create({PopoverNotificationPage,  { cssClass: 'custom-popover' }});
     popover.present({ ev: myEvent });
   }
+  logout() {
+    console.log("Logout: " );
+
+    this.authServerProvider.logout().subscribe((data) => {
+      console.log("Logout: " + data);
+      // this.app.getRootNavs()[0].setRoot(loginPage);
+      var nav = this.app.getRootNav();
+      console.log(nav);
+      nav.setRoot(FirstRunPage);
+    }, (err) => {
+    });
+  }
+
 }
