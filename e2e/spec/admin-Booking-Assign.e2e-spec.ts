9caddbcd2732ae9a8b347d1926ff3d4f4a4cdd73
Unknown <farwaj1@gmail.com>
2019-02-11 23:05:37 +0000

    Admin Booking Assign - e2e Test

---
+++ e2e/spec/admin-Booking-Assign.e2e-spec.ts
@@ -0,0 +1,36 @@
+import { AdmingBookingAssignP } from './../pages/admin-booking-assign.po';
+import { browser } from 'protractor';
+
+describe('AdmingBookingAssignPage', () => {
+  let adminBookingAssignPage;
+
+  beforeEach(() => {
+    adminBookingAssignPage = new AdmingBookingAssignP();
+    adminBookingAssignPage.navigateTo('/#/admin-booking-assign');
+    browser.waitForAngular();
+  });
+
+  
+  it('should show an Assign Tutor button', () => {
+    expect(adminBookingAssignPage.assignButton.isPresent());
+});
+
+  
+it('should show page being scrolled up', () => {
+  browser.executeScript('window.scrollTo(0,0);').then(function() {
+    browser.sleep(3000);
+  })
+});
+
+it('should show page being scrolled down', () => {
+  browser.executeScript('window.scrollTo(0,500);').then(function() {
+    browser.sleep(3000);
+  })
+});
+
+
+// it('Total Items Per Page should be 8', () => {
+//   expect(adminBookingAssignPage.list.count()).toBe(8);
+// });
+
+});
