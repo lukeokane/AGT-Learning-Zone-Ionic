603e052b3f4caaee8c6c94aa477b344cc3a8a3cd
elaine chong <elainepeilingchong@gmail.com>
2019-02-13 18:11:41 +0000

    Displayed Available Time slot dynamically

--- src/pages/user-request-timeslot/user-request-timeslot.ts
+++ src/pages/user-request-timeslot/user-request-timeslot.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams, ModalController, ViewController } from 'ionic-angular';
 import { Booking } from '../../class/Booking';
+import { AvailableTime } from '../../class/AvailableTime';
 
 /**
  * Generated class for the UserRequestTimeslotPage page.
@@ -19,8 +20,11 @@ export class UserRequestTimeslotPage {
   timeSelected: any;
   booking: Booking;
   dateStart: any;
-  dateEnd: any;  
-  
+  dateEnd: any;
+  availableTimes: Array<{ date: Date, time: Array<AvailableTime> }>;
+  days: String[] = ["Sun", "Mon", "Tue", "Wed", "Thurs", "Fri", "Sat"];
+  selectedTime: Array<AvailableTime>;
+
   constructor(public navCtrl: NavController, public navParams: NavParams, private modalCtrl: ModalController, private viewCtrl: ViewController) {
     if (this.navParams.get("booking") != null && this.navParams.get("booking") != undefined)
       this.booking = this.navParams.get("booking");
@@ -31,6 +35,9 @@ export class UserRequestTimeslotPage {
     this.timeSelected = this.navParams.get("timeSelected");
     this.dateStart = this.navParams.get("dateStart");
     this.dateEnd = this.navParams.get("dateEnd");
+    this.availableTimes = this.navParams.get("availableTimes");
+
+    this.selectedTime = new Array();
   }
 
   ionViewDidLoad() {
@@ -38,18 +45,47 @@ export class UserRequestTimeslotPage {
   }
 
   onClickRequest() {
+    this.convertAllSelectedTime();
     let confirmModal = this.modalCtrl.create("UserRequestSendTutorialPage", { booking: this.booking });
     confirmModal.onDidDismiss(data => {
       // return send ; true=send
-      if(data!=undefined && data !=null){
-        if(data==true){
-//send to backend
+      if (data != undefined && data != null) {
+        // if (data == true) {
+        //   //send to backend
 
-        }
-        this.viewCtrl.dismiss(data);
+        // }
+        this.viewCtrl.dismiss({send:data,booking:this.booking});
       }
     });
     confirmModal.present();
   }
 
+  convertAllSelectedTime() {
+    let s = "";
+    this.selectedTime.forEach((value, index) => {
+      s = s + value.startTime.toISOString().substring(0, 19) + "Z|" + value.endTime.toISOString().substring(0, 19) + "Z"
+      if (index != this.selectedTime.length - 1) {
+        s += "&"
+      }
+    });
+    this.booking.requestTimes = s;
+  }
+  onSelectTimeSlot(t: AvailableTime) {
+    let i = this.selectedTime.findIndex(value => t.startTime.toISOString().substring(0, 21) == value.startTime.toISOString().substring(0, 21));
+    if (i == -1) {
+      this.selectedTime.push(t);
+
+    } else {
+      this.selectedTime.splice(i, 1);
+    }
+    console.log(this.selectedTime);
+  }
+  checkSelected(t: AvailableTime) {
+    if (this.selectedTime.some(value => t.startTime.toISOString().substring(0, 21) == value.startTime.toISOString().substring(0, 21))) {
+      return "time-slot-selected-btn";
+    } else {
+      return "time-slot-btn";
+    }
+
+  }
 }
