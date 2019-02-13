34fc61e4f80dc41fd3ad998be5bc6c0ab4819abd
Unknown <farwaj1@gmail.com>
2019-02-13 23:27:08 +0000

    Quagga Init - Start Camera LiveStream

--- src/pages/itlc/itlc-modal-checkin/itlc-modal-checkin.module.ts
+++ src/pages/itlc/itlc-modal-checkin/itlc-modal-checkin.module.ts
@@ -1,3 +1,4 @@
+import { TranslateModule } from '@ngx-translate/core';
 import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
 import { ItlcModalCheckinPage } from './itlc-modal-checkin';
@@ -8,6 +9,7 @@ import { ItlcModalCheckinPage } from './itlc-modal-checkin';
   ],
   imports: [
     IonicPageModule.forChild(ItlcModalCheckinPage),
+    TranslateModule.forChild()
   ],
 })
 export class ItlcModalCheckinPageModule {}
--- src/pages/itlc/itlc-modal-checkin/itlc-modal-checkin.ts
+++ src/pages/itlc/itlc-modal-checkin/itlc-modal-checkin.ts
@@ -1,25 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
-
-/**
- * Generated class for the ItlcModalCheckinPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
+import Quagga from 'quagga'; 
 
 @IonicPage()
 @Component({
   selector: 'page-itlc-modal-checkin',
   templateUrl: 'itlc-modal-checkin.html',
 })
-export class ItlcModalCheckinPage {
+export class ItlcModalCheckinPage implements OnInit {
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad ItlcModalCheckinPage');
-  }
+  ngOnInit() {
 
+    Quagga.init({
+      inputStream: {
+        name: "Live",
+        type: "LiveStream",
+        target: document.querySelector('#scanner'),
+        constraints: {
+          width: 640,
+          height: 280,
+          facing: "environment"
+      }
+      },
+      decoder: {
+        readers: [
+          "code_128_reader"
+        ],
+      },
+
+    }, function (err) {
+      if (err) {
+        console.log(err);
+        return
+      }
+
+      Quagga.start();
+
+    });
+  }
 }
