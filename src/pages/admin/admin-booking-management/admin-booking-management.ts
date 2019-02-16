9564859b69506fa8d3ebc58cb2a1c3e58c30b19b
Unknown <farwaj1@gmail.com>
2019-02-16 22:54:45 +0000

    Booking No comment - updated

--- src/pages/admin/admin-booking-management/admin-booking-management.ts
+++ src/pages/admin/admin-booking-management/admin-booking-management.ts
@@ -40,6 +40,7 @@ export class AdminBookingManagementPage implements OnInit {
   startTime: any;
   endTime: any;
   result = [];
+  empty:any;
 
   constructor(public navCtrl: NavController, public navParams: NavParams, private bookingsService: BookingsService, private toastCtrl: ToastController, private userService: UserService, private semesterGroupService: SemesterGroupService, private subjectService: SubjectsService, private bookingService: BookingsService) {
   }
@@ -189,8 +190,13 @@ export class AdminBookingManagementPage implements OnInit {
 
   }
 
+
   assignToTutor(selectedBooking: Booking, filterTutors: Array<User> = []) {
     let rand = filterTutors[Math.floor(Math.random() * filterTutors.length)];
+    if(selectedBooking.userComments == '')
+    {
+      this.empty = true;
+    }
     if (rand != null || rand != undefined) {
       selectedBooking.adminAcceptedId = rand.id;
     }
