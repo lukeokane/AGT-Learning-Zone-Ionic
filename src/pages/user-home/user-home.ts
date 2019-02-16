3d82614e3ec0c624177c348d6c5f81048a97f7de
elaine chong <elainepeilingchong@gmail.com>
2019-02-16 21:27:05 +0000

    Updated user home calendar

--- src/pages/user-home/user-home.ts
+++ src/pages/user-home/user-home.ts
@@ -80,79 +80,90 @@ export class UserHomePage {
     var timeInt = parseInt(time, 10);
     var timeS1 = "" + timeInt;
     if (timeInt < 10) {
-      timeS1 = "0" + timeSelected + "";
+      timeS1 = "0" + timeInt + "";
+    }
+    var timeInt2 = timeInt + 1;
+    var timeS2 = "" + timeInt;
+    if (timeInt2 < 10) {
+      timeS2 = "0" + timeInt2 + "";
     }
     let s = dateSelected.toISOString().substring(0, 10) + "T" + timeS1 + ":00:00Z";
-    if (!(this.bookings.some((value, index, array) => {
-      return typeof (value.booking.startTime) == "string" ? value.booking.startTime.substring(0, 19) == s.substring(0, 19) : value.booking.startTime.toISOString() == s.substring(0, 19);
-    }))) {
-      let toast;
-      let profileModal = this.modalCtrl.create("UserRequestModalPage", { dateSelected: dateSelected, timeSelected: timeInt, bookings: this.bookings });
+    let s2 = dateSelected.toISOString().substring(0, 10) + "T" + timeS1 + ":00:00Z";
 
-      profileModal.onDidDismiss(data => {
-        if (data != undefined && data != null) {
-          if (data.send) {
-            this.bookingService.create(data.booking).subscribe(data => {
-              toast = this.toastCtrl.create({
-                message: 'Thank You! You will receive a confirmation e-mail when your request is approved',
-                duration: 5000,
-                position: 'top',
-                showCloseButton: true,
-                closeButtonText: "Close"
-              });
-              toast.present();
+    if (new Date() >= new Date(s2)) {
+      console.log("BIG");
+    }
+    else {
 
-            }, (erro) => {
-              toast = this.toastCtrl.create({
-                message: 'Your request is not sent. Please try again',
-                duration: 5000,
-                position: 'top',
-                showCloseButton: true,
-                closeButtonText: "Close"
-              });
-              toast.present();
-              console.error(erro);
-            })
-            // send to backend
-
-          }
-        }
-      });
-      profileModal.present();
-    } else {
-      let i = this.bookings.findIndex(value => {
+      if (!(this.bookings.some((value, index, array) => {
         return typeof (value.booking.startTime) == "string" ? value.booking.startTime.substring(0, 19) == s.substring(0, 19) : value.booking.startTime.toISOString() == s.substring(0, 19);
-      });
-      if (i != -1) {
-        console.log(this.bookings[i]);
-        let profileModal = this.modalCtrl.create("UserJoinTutorialModalPage", { dateSelected: dateSelected, timeSelected: timeInt, booking: this.bookings[i].booking });
+      }))) {
+        let toast;
+        let profileModal = this.modalCtrl.create("UserRequestModalPage", { dateSelected: dateSelected, timeSelected: timeInt, bookings: this.bookings });
+
         profileModal.onDidDismiss(data => {
-          if (data != null && data != undefined) {
-            if (data.send == true) {
-              let toast = this.toastCtrl.create({
-                message: 'Your request is sent',
-                duration: 5000,
-                position: 'top',
-                showCloseButton: true,
-                closeButtonText: "Close"
-              });
-              toast.present();
-            }else{
-              let toast = this.toastCtrl.create({
-                message: 'Error occur, please try again later',
-                duration: 5000,
-                position: 'top',
-                showCloseButton: true,
-                closeButtonText: "Close"
-              });
-              toast.present();
+          if (data != undefined && data != null) {
+            if (data.send) {
+              this.bookingService.create(data.booking).subscribe(data => {
+                toast = this.toastCtrl.create({
+                  message: 'Thank You! You will receive a confirmation e-mail when your request is approved',
+                  duration: 5000,
+                  position: 'top',
+                  showCloseButton: true,
+                  closeButtonText: "Close"
+                });
+                toast.present();
+
+              }, (erro) => {
+                toast = this.toastCtrl.create({
+                  message: 'Your request is not sent. Please try again',
+                  duration: 5000,
+                  position: 'top',
+                  showCloseButton: true,
+                  closeButtonText: "Close"
+                });
+                toast.present();
+                console.error(erro);
+              })
+              // send to backend
+
             }
           }
         });
         profileModal.present();
+      } else {
+        let i = this.bookings.findIndex(value => {
+          return typeof (value.booking.startTime) == "string" ? value.booking.startTime.substring(0, 19) == s.substring(0, 19) : value.booking.startTime.toISOString() == s.substring(0, 19);
+        });
+        if (i != -1) {
+          let profileModal = this.modalCtrl.create("UserJoinTutorialModalPage", { dateSelected: dateSelected, timeSelected: timeInt, booking: this.bookings[i].booking });
+          profileModal.onDidDismiss(data => {
+            if (data != null && data != undefined) {
+              if (data.send == true) {
+                let toast = this.toastCtrl.create({
+                  message: 'Your request is sent',
+                  duration: 5000,
+                  position: 'top',
+                  showCloseButton: true,
+                  closeButtonText: "Close"
+                });
+                toast.present();
+              } else {
+                let toast = this.toastCtrl.create({
+                  message: 'Error occur, please try again later',
+                  duration: 5000,
+                  position: 'top',
+                  showCloseButton: true,
+                  closeButtonText: "Close"
+                });
+                toast.present();
+              }
+            }
+          });
+          profileModal.present();
 
+        }
       }
-
 
     }
 
@@ -187,7 +198,7 @@ export class UserHomePage {
     return Math.ceil((((d - yearStart) / 86400000) + 1) / 7)
   }
   getStartEndDate() {
-    return this.dates[0].getDate() + " " + this.months[this.dates[0].getMonth()] + " - " + this.dates[this.dates.length-1].getDate() + " " + this.months[this.dates[this.dates.length-1].getMonth()];
+    return this.dates[0].getDate() + " " + this.months[this.dates[0].getMonth()] + " - " + this.dates[this.dates.length - 1].getDate() + " " + this.months[this.dates[this.dates.length - 1].getMonth()];
   }
   getMonday(d: Date) {
     d = new Date(d);
@@ -196,7 +207,7 @@ export class UserHomePage {
     return new Date(d.setDate(diff));
   }
   onNextWeek() {
-    var nextMonday = new Date(this.dates[this.dates.length-1].getTime());
+    var nextMonday = new Date(this.dates[this.dates.length - 1].getTime());
     nextMonday.setTime(nextMonday.getTime() + (24 * 60 * 60 * 1000));
     this.generateDate(nextMonday);
 
