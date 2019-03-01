fb1dc84a904a7d37933a3b5ea69f6e763143522d
Unknown <farwaj1@gmail.com>
2019-03-01 16:26:42 +0000

    Register as (role) Tutor added to Registration Page

--- src/pages/signup/signup.ts
+++ src/pages/signup/signup.ts
@@ -17,15 +17,16 @@ import { Course } from '../../class/Course';
 })
 export class SignupPage implements OnInit {
   // The account fields for the signup form
-  account: { login: string, email: string, firstName: string, lastName: string, password: string, langKey: string, course: string, year: number } = {
+  account: { login: string, email: string, firstName: string, lastName: string, password: string, langKey: string, courseId: number, year: number,authorities: any[] } = {
     login: '',
     email: '',
     firstName: '',
     lastName: '',
     password: '',
     langKey: 'en',
-    course: '',
-    year: 0
+    courseId: 0,
+    year: 0,
+    authorities: [''],
   };
 
   // Our translated text strings
@@ -44,6 +45,8 @@ export class SignupPage implements OnInit {
   queryCount: any;
   itemsPerPage: any;
   selectedYear: CourseYear;
+  selectedCourse :Course;
+  roleType: any;
 
   constructor(public navCtrl: NavController,
     public user: User,
@@ -113,13 +116,17 @@ export class SignupPage implements OnInit {
   }
 
   doSignup() {
-    this.account.year = this.selectedYear.id;
-    console.log(this.account);
-    console.log(this.selectedYear);
+    if (this.roleType == "ROLE_TUTOR") {
+      this.account.authorities = ['ROLE_TUTOR'];
+      this.account.year = null;
+    }
+    else if (this.roleType == "ROLE_USER") {
+      this.account.authorities = ['ROLE_USER'];
+      this.account.year = this.selectedYear.id;
+      this.account.courseId=this.selectedCourse.id;
+    }
 
-    // set login to same as email
     this.account.login = this.account.email;
-    // Attempt to login in through our User service
     this.user.signup(this.account).subscribe(() => {
       let toast = this.toastCtrl.create({
         message: this.signupSuccessString,
@@ -151,4 +158,7 @@ export class SignupPage implements OnInit {
     return window.innerWidth;
   }
 
+  roleTypes(event: any) {
+    this.roleType = event;
+  }
 }
