d654e50943a7a1071899ffec19c6c2ca7000a1e4
Unknown <farwaj1@gmail.com>
2019-03-23 01:05:11 +0000

    Message Class Created & imported Components Module for top menu

---
+++ src/class/Message.ts
@@ -0,0 +1,11 @@
+export class Message {
+
+    constructor(
+        public id?:number,
+        public tag?:string,
+        public reason?:string,
+        public content?:string
+    )
+    { }
+
+}
