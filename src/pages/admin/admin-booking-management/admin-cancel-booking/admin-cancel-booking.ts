db5a855a8ccad1fa789045661583bf37dc2ecf3f
Unknown <farwaj1@gmail.com>
2019-03-24 20:27:15 +0000

    Display a list of cancelling messages in a list

--- src/pages/admin/admin-booking-management/admin-cancel-booking/admin-cancel-booking.ts
+++ src/pages/admin/admin-booking-management/admin-cancel-booking/admin-cancel-booking.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { MessagesService } from './../../../../services/Message.provider';
+import { Message } from './../../../../class/Message';
+import { Component, OnInit } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
 @IonicPage()
@@ -6,13 +8,35 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   selector: 'page-admin-cancel-booking',
   templateUrl: 'admin-cancel-booking.html',
 })
-export class AdminCancelBookingPage {
+export class AdminCancelBookingPage implements OnInit{
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  messages : Array<Message>=[];
+  message:any;
+  tag:string;
+  other:any;
+
+
+  constructor(public navCtrl: NavController, public navParams: NavParams,private messageService:MessagesService) {
   }
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad AdminCancelBookingPage');
+  ngOnInit()
+  {
+    this.initMessages();
   }
+
+  initMessages()
+  {
+    this.tag = "cancelBooking";
+    this.messageService.query().subscribe(data => {
+
+      data.forEach(message => {
+        if(message.tag == this.tag){
+          this.messages.push(message);
+          console.log("aaaaaaa ", this.messages);
+        }
+      });
+    }, error => console.log(error));
+  }
+  
 
 }
