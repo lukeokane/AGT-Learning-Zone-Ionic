525b774af17e26a8a5c01d98c7405d37490f2752
Unknown <farwaj1@gmail.com>
2019-04-11 22:38:53 +0100

    Same error fixed in admin booking assign page

--- src/pages/admin/admin-booking-assign/admin-booking-assign.ts
+++ src/pages/admin/admin-booking-assign/admin-booking-assign.ts
@@ -57,7 +57,7 @@ export class AdminBookingAssignPage implements OnInit {
 
   initUsers() {
     this.itemsPerPage =  20;
-    this.userService.getAllUsers(
+    this.userService.getAllTutors(
       {
         page: this.page - 1,
         size: this.itemsPerPage,
