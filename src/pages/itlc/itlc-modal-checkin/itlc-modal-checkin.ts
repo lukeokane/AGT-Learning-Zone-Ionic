50295b8360525751eb217fb0164edddd10a71e91
Unknown <farwaj1@gmail.com>
2019-04-18 21:13:37 +0100

    Removed Quagga from Checkin

--- src/pages/itlc/itlc-modal-checkin/itlc-modal-checkin.ts
+++ src/pages/itlc/itlc-modal-checkin/itlc-modal-checkin.ts
@@ -30,83 +30,9 @@ export class ItlcModalCheckinPage implements OnInit {
    }
 
   ngOnInit() {
-    let video: any = document.getElementById('video');
-    if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
-      navigator.mediaDevices.getUserMedia({ video: true }).then(function (stream) {
-        video.srcObject = stream;
-        video.play();
-      });
 
-    }
-
-    // Elements for taking the snapshot
-    let canvas: any = document.getElementById('canvas');
-    let context: any = canvas.getContext('2d');
-    video = document.getElementById('video');
-
-    Quagga.init({
-      inputStream: {
-        name: "Live",
-        type: "LiveStream",
-        target: document.querySelector('#scanner'),
-        constraints: {
-          width: 570,
-          height: 260,
-          facingMode: "user",
-        },
-      },
-      decoder: {
-        readers: [
-          "code_39_reader",
-          "code_39_vin_reader",
-        ],
-      },
-
-    }, function (err) {
-      if (err) {
-        console.log(err);
-        return
-      }
-
-      Quagga.start();
-    });
-
-    Quagga.onProcessed(function (result) {
-
-      var drawingCtx = Quagga.canvas.ctx.overlay,
-        drawingCanvas = Quagga.canvas.dom.overlay;
-      if (result) {
-
-        if (result.boxes) {
-          drawingCtx.clearRect(0, 0, parseInt(drawingCanvas.getAttribute("width")), parseInt(drawingCanvas.getAttribute("height")));
-          result.boxes.filter(function (box) {
-            return box !== result.box;
-          }).forEach(function (box) {
-            Quagga.ImageDebug.drawPath(box, { x: 0, y: 3 }, drawingCtx, { color: "green", lineWidth: 2 });
-          });
-        }
-
-        if (result.box) {
-          Quagga.ImageDebug.drawPath(result.box, { x: 0, y: 1 }, drawingCtx, { color: "#00F", lineWidth: 2 });
-        }
-
-        if (result.codeResult && result.codeResult.code) {
-          Quagga.ImageDebug.drawPath(result.line, { x: 'x', y: 'y' }, drawingCtx, { color: 'red', lineWidth: 1 });
-        }
-      }
-    });
-
-
-    Quagga.onDetected((result) => {
-      console.log("Barcode detected and processed : [" + result.codeResult.code + "]", result);
-      // document.querySelector(".found").innerHTML = result.codeResult.code;
-      this.login =result.codeResult.code;
-      context.drawImage(video, 20, 16.5,575, 320);
-      Quagga.stop();
-      this.checkInUserDetails(this.login);
-    },
-    );
   }
+
 
   checkInUserDetails(barcode :any)
   { 
