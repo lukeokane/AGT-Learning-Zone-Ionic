12af130b393fd34ef6c31c1655e54a1afc2575f6
Unknown <farwaj1@gmail.com>
2019-04-18 23:35:16 +0100

    Added Event listener to catch result of barcode scan & Added Gif to Checkin Modal

--- src/pages/itlc/itlc-modal-checkin/itlc-modal-checkin.ts
+++ src/pages/itlc/itlc-modal-checkin/itlc-modal-checkin.ts
@@ -14,64 +14,76 @@ import { itlcHomePage } from '../../pages';
 })
 export class ItlcModalCheckinPage implements OnInit {
   login: any;
-  user:any;
-  selectedBooking:Booking = new Booking();
-  clicked:boolean=false;
+  user: any;
+  selectedBooking: Booking = new Booking();
+  clicked: boolean = false;
   constructor(
     public navParams: NavParams,
     public navCtrl: NavController,
     private userService: UserService,
-    private bookingUserDetailService:BookingUserDetailService,
-    private viewCtrl:ViewController
+    private bookingUserDetailService: BookingUserDetailService,
+    private viewCtrl: ViewController
   ) {
     if (navParams.get('selectedBooking') != null || navParams.get('selectedBooking') != undefined) {
       this.selectedBooking = navParams.get('selectedBooking');
     }
-   }
+  }
 
   ngOnInit() {
-
+    //automatic click to collect result
+    document.getElementById("modal").click();
+    let code = "";
+    window.addEventListener('keypress', e => {
+      //Because our scanner throws an 'Enter' key at the end of scanning
+      if (e.keyCode === 13) {
+          console.log("Scanned Result ", code);
+        this.login = code;
+        this.checkInManual();
+      } else {
+        code += e.key;        
+      }
+    })
   }
 
 
-  checkInUserDetails(barcode :any)
-  { 
+  checkInUserDetails(barcode: any) {
     this.userService.getUserByLogin(barcode).subscribe(user => {
-      this.user= user;
-      console.log("this user",this.user);
+      this.user = user;
+      console.log("this user", this.user);
     }, (error) => {
       console.error(error);
     });
   }
 
-  checkIn()
-  {
+  checkIn() {
     let bookingUserDetails
-    bookingUserDetails= new BookingUserDetails();
-    if(this.selectedBooking != null || this.selectedBooking != undefined)
-    {
-    this.bookingUserDetailService.updateBookingUserDetailsForCheckIn(this.selectedBooking.id,this.login,bookingUserDetails).subscribe(data => {
-      console.log("data",data);
-      this.navCtrl.push(itlcHomePage);
-    }, (error) => {
-      console.error(error);
-    });
-  }
+    bookingUserDetails = new BookingUserDetails();
+    if (this.selectedBooking != null || this.selectedBooking != undefined) {
+      this.bookingUserDetailService.updateBookingUserDetailsForCheckIn(this.selectedBooking.id, this.login, bookingUserDetails).subscribe(data => {
+        console.log("data", data);
+        this.navCtrl.push(itlcHomePage);
+      }, (error) => {
+        console.error(error);
+      });
+    }
   }
 
-  cancel()
-  {
+  cancel() {
     this.viewCtrl.dismiss();
   }
 
-  manualCheckIn()
-  {
-    this.clicked=true;
+  manualCheckIn() {
+    this.clicked = true;
   }
-  checkInManual()
-  {
-    this.login= (<HTMLInputElement>document.getElementById("input_id")).value;
+  checkInManual() {
+    if(<HTMLInputElement>document.getElementById("input_id") != null || <HTMLInputElement>document.getElementById("input_id") != undefined)
+    {
+   this.login = (<HTMLInputElement>document.getElementById("input_id")).value;
+    }
+    if(this.login != null || this.login != undefined)
+    {
     this.checkInUserDetails(this.login);
+    }
   }
 }
 
